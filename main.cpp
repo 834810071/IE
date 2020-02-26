5b4bddf53d271fa296880066d4c557ead0bb4bb2
834810071 <834810071@qq.com>
2020-02-26 20:52:01 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <memory>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+void test();
+
+int main()
+{
+    test();
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by jxq on 20-2-26.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int x;
+
+void test() {
+    cout << x << endl;
+}
