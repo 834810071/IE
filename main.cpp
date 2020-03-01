596dc71d6a8a43d82d7770bd7e4abe4c269841fc
834810071 <834810071@qq.com>
2020-03-01 19:09:41 +0800

    day04

--- main.cpp
+++ main.cpp
@@ -1,65 +1,32 @@
-#include <memory>
 #include <iostream>
-#include <string>
-#include <stdio.h>
-#include <cstring>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
+class A {
 
-void test();
+public:
+    A() {
+        cout << "A create！" << endl;
+    }
 
-int selectPivot(vector<int>& arr, int s, int e) {
-    int mid = (s + e) / 2;
-    int a[3] = {arr[s], arr[mid], arr[e]};
-    sort(a, a+3);
-    return a[0];
+    ~A() {
+        cout << "A delete" << endl;
+    }
+
+    A& operator=(const A& a){
+        cout << "赋值" << endl;
+        return *this;
+    }
+
+    A(const A& a){
+        cout << "拷贝" << endl;
+    }
 };
 
-
-
-int quick_sort(vector<int>& arr, int s, int e) {
-
-    int pivot = selectPivot(arr, s, e);
-    while (s < e) {
-        while (s < e && arr[e] >= pivot)
-            --e;
-        if (s < e) {
-            int tmp = arr[s];
-            arr[s] = arr[e];
-            arr[e] = tmp;
-        }
-        while (s < e && arr[s] <= pivot) {
-            ++s;
-        }
-
-        if (s < e) {
-            int tmp = arr[s];
-            arr[s] = arr[e];
-            arr[e] = tmp;
-        }
-    }
-    arr[s] = pivot;
-    return s;
-}
-
-void quick(vector<int>& arr, int s, int e) {
-    if (s >= e) {
-        return ;
-    }
-    int p = quick_sort(arr, s, e);
-    quick(arr, s, p-1);
-    quick(arr, p+1, e);
-}
-
-int main()
-{
-    vector<int> arr = {1,2,5,6,0,3,5,8,9,10,3,6};
-    quick(arr, 0, arr.size()-1);
-    for (auto& i : arr) {
-        cout << i << " ";
-    }
+int main() {
+    A a;
+    A b = a;
+    A c(a);
+    b = c;
     return 0;
 }
