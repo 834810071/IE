433dc74b2887351b25832d53cdc29046a68c6788
834810071 <834810071@qq.com>
2020-03-05 19:23:41 +0800

    day06

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # IE
 面经记录
+
+推荐： [C/C++ 技术面试基础知识总结，包括语言、程序库、数据结构、算法、系统、网络、链接装载库等知识及面试经验、招聘、内推等信息。](https://github.com/huihut/interview)
+
+[后台开发相关知识](https://github.com/twomonkeyclub/BackEnd)
+
+[计算机基础（计算机网络/操作系统/数据库/Git...）面试问题全面总结，包含详细的follow-up question以及答案；全部采用【问题+追问+答案】的形式，即拿即用，直击互联网大厂面试rocket；可用于模拟面试、面试前复习、短期内快速备战面试...](https://github.com/wolverinn/Waking-Up)
+
+[面试常见问题汇总](https://github.com/Making-It/CS_Review)
+
+[2020 grinninggringrinninggringrinning 秋招笔试面试合集，以及2019届公司真题模拟题集 & 附上自己的后端指南，computermortar_board包括（C/C++基础、数据结构、算法、操作系统computer、计算机网络、MySQL、shell（ps：sed、awk、grep））](https://github.com/Apriluestc/2020)
+
+[笔试面试知识整理](https://github.com/HIT-Alibaba/interview)
--- main.cpp
+++ main.cpp
@@ -1,40 +1,62 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
 
 using namespace std;
 
-class A {
+int Mypartition(vector<int>& v, int l, int r) {
+    int pivot = v[l];
+    while (l < r) {
+        while (l < r && v[r] > pivot) {
+            --r;
+        }
+        if (l < r) {
+            int t = v[r];
+            v[r] = v[l];
+            v[l] = t;
+        }
 
-public:
-    A() {
-        cout << "A create！" << endl;
+        while (l < r && v[l] < pivot) {
+            ++l;
+        }
+
+        if (l < r) {
+            int t = v[r];
+            v[r] = v[l];
+            v[l] = t;
+        }
     }
 
-    ~A() {
-        cout << "A delete" << endl;
+    v[l] = pivot;
+    return l;
+}
+
+
+
+void quick_sort(vector<int>& v, int l, int r) {
+    if (l >= r) {
+        return ;
     }
 
-    A& operator=(const A& a){
-        cout << "赋值" << endl;
-        return *this;
-    }
+    int p = Mypartition(v, l, r);
+    quick_sort(v, l, p-1);
+    quick_sort(v, p+1, r);
+}
 
-    A(const A& a){
-        cout << "拷贝" << endl;
-    }
-};
 
 int main() {
-    char* a = "你";
-    char* b = "1";
-    cout << sizeof(*a) << endl;
-    cout << sizeof(*b) << endl;
-
-    cout << sizeof(a) << endl;
-    cout << sizeof(b) << endl;
-
-    cout << typeof(a) << endl;
-    cout << strlen(b) << endl;
-
+    vector<int> v;
+    v.push_back(0);
+    v.push_back(2);
+    v.push_back(1);
+    v.push_back(5);
+    v.push_back(4);
+    v.push_back(9);
+    v.push_back(7);
+    v.push_back(8);
+    quick_sort(v, 0, v.size()-1);
+    for (auto& a : v) {
+        cout << a << " ";
+    }
+    cout << endl;
     return 0;
 }
